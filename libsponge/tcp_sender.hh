c3b0c3a3e29e4774d67dd7eca4ce916ec39db03f
rookiebepaient <chen18249881210@163.com>
2023-03-07 18:20:28 -0800

    finish Lab3 TCP Sender

--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -6,8 +6,10 @@
 #include "tcp_segment.hh"
 #include "wrapping_integers.hh"
 
+#include <deque>
 #include <functional>
 #include <queue>
+#include <utility>
 
 //! \brief The "sender" part of a TCP implementation.
 
@@ -17,6 +19,31 @@
 //! segments if the retransmission timer expires.
 class TCPSender {
   private:
+    //! retransmission timer
+    class Timer {
+      private:
+        size_t _rto;
+
+      public:
+        //! Initialize a Timer
+        Timer(size_t &time_ms) : _rto(time_ms) {}
+
+        //! change _rto value
+        void change_rto(size_t time_ms) {
+            if (_rto >= time_ms) {
+                _rto = _rto - time_ms;
+            } else {
+                _rto = 0;
+            }
+        }
+
+        //! set _rto value
+        void set_rto(size_t time_ms) { _rto = time_ms; }
+
+        //! get _rto value
+        size_t get_rto() { return _rto; }
+    };
+
     //! our initial sequence number, the number for our SYN.
     WrappingInt32 _isn;
 
@@ -31,6 +58,44 @@ class TCPSender {
 
     //! the (absolute) sequence number for the next byte to be sent
     uint64_t _next_seqno{0};
+
+    // follow is self-private member
+
+    //! the receiver windowsize
+    size_t _receiver_windowsize{1};
+
+    //! store the sent but no acked seg
+    std::deque<std::pair<TCPSegment, TCPSender::Timer>> _seg_timer_pair{};
+
+    //! consecutive_retransmissions
+    size_t _consecutive_retransmissions{0};
+
+    //! retx_timeout
+    size_t _retx_timeout{0};
+
+    //! eof sign
+    bool _eof_sign{false};
+
+    //! zero windossize
+    bool _is_zero_ws{false};
+
+    //！是否建立连接发送SYN
+    bool _is_syn_sent{false};
+
+    //! 是否发送FIN
+    bool _is_fin_sent{false};
+
+    //! 已发送出去但未被确认的字节个数
+    size_t _bytes_in_flight{0};
+
+    //! 计时器，用来监视是否需要超时重传
+    size_t _retransmission_timer{0};
+
+    //！
+    size_t _is_timer_ruuning{false};
+
+    //! 已发送但是未被确认的报文段
+    std::deque<TCPSegment> _outstanding_segments{};
 
   public:
     //! Initialize a TCPSender
